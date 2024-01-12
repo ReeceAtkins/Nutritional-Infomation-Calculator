20d5ed8d3eed5ae0a0021cedfb0a9fabff3afc24
Reece Atkins <Reecepowered2@hotmail.com>
2024-01-11 20:36:15 -0800

    Add project files.

---
+++ Nutritional Infomation Calculator/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Nutritional_Infomation_Calculator.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
