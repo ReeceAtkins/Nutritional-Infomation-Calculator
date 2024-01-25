3d6439fe08826e1b802afbfcd580aefb631317d1
Reece Atkins <Reecepowered2@hotmail.com>
2024-01-25 13:18:36 -0800

    Created first migration and database

---
+++ Nutritional Infomation Calculator/Data/MenuContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Nutritional_Infomation_Calculator.Models;
+
+namespace Nutritional_Infomation_Calculator.Data
+{
+    public class MenuContext : DbContext
+    {
+        public MenuContext(DbContextOptions<MenuContext> options)
+                : base(options)
+        {
+        }
+
+        public DbSet<MenuItem> MenuItems { get; set; }
+    }
+}
